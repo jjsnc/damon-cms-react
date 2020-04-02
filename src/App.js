6aa7129b527c76c1b47315bac2c4ee422083677a
Damon <zhuowangyi@163.com>
2020-04-02 09:56:11 +0800

    antd 引入测试

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+// import 'antd/dist/antd.css';
+import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-       hello world
+      <Button type="primary">Primary</Button>
+      <Button>Default</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="link">Link</Button>
     </div>
   );
 }
