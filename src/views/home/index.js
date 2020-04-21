32d67f4bb67b29f14ae67d51f600cf1e745e0d64
Damon <zhuowangyi@163.com>
2020-04-21 11:53:02 +0800

    init

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,28 +1,23 @@
-
 import React from 'react';
 import { connect } from 'react-redux';
-import { actionCreators } from './store';
 class Home extends React.Component {
+    componentDidMount() {
+    }
     render() {
 
         return (
             <div>
-              home
+                home
             </div>
         )
     }
-    componentDidMount() {
-        this.props.changeHomeData();
-    }
+
 }
 
 const mapState = (state) => ({
-    topicList: state.getIn(['home', 'topicList']),
 })
 const mapDispatch = (dispatch) => ({
-    changeHomeData() {
-        dispatch(actionCreators.getHomeInfo());
-    },
+
 });
 
 export default connect(mapState, mapDispatch)(Home);
